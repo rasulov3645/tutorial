3c8b962f1111e26ae2034f18a5a72b47bf4d3614
Расулов Гамзат <rasuolov3645@gmail.com>
2017-07-06 02:50:56 +0300

    How to Define a Custom Django Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    description = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+    website = models.URLField(default='')
+    phone = models.IntegerField(default=0)
