ddf4835e2e1b4acb1361ba0fa955c5960b9f743c
Расулов Гамзат <rasuolov3645@gmail.com>
2017-07-06 14:11:31 +0300

    Creating the Django User Registration Form

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def home(request):
@@ -7,3 +9,15 @@ def home(request):
 
     args = {'myName': name, 'numbers': numbers}
     return render(request, 'accounts/home.html', args)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    else:
+        form = UserCreationForm()
+
+        args = {'form': form}
+        return render(request, 'accounts/reg_form.html', args)
