31b3eccbb1de8d5b0555ed08c47412e9a3b101e1
Расулов Гамзат <rasuolov3645@gmail.com>
2017-07-06 04:49:00 +0300

    Preparing URLs for the Django Registration System

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url, include
 from django.contrib import admin
+from tutorial import views
+
 
 urlpatterns = [
+    url(r'^$', views.login_redirect, name='login_redirect'),
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('accounts.urls')),
 ]
